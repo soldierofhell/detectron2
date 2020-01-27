475ab0b1ac745df2cc9237b95c1f5f73d036d89c
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-01-27 14:41:35 +0100

    Update custom_train_net.py

--- tools/custom_train_net.py
+++ tools/custom_train_net.py
@@ -220,7 +220,7 @@ def main(args):
         return do_test(cfg, model)
 
     optimizer = build_optimizer(cfg, model)
-    model, optimizer = amp.initialize(model, optimizer, opt_level='O0') # 'O1'
+    #model, optimizer = amp.initialize(model, optimizer, opt_level='O0') # 'O1'
     distributed = comm.get_world_size() > 1
     if distributed:
         model = DistributedDataParallel(
