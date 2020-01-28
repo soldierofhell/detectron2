9c652a7de36572a10561de75992d2ebc802686e9
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-01-28 10:12:01 +0100

    Update apex_trainer.py

--- detectron2/engine/apex_trainer.py
+++ detectron2/engine/apex_trainer.py
@@ -7,19 +7,34 @@ from apex import amp
 
 
 class ApexTrainer(DefaultTrainer):
-    def __init__(self, cfg, optimization_level):
+    def __init__(self, cfg):
+        """
+        Args:
+            cfg (CfgNode):
+        """
         logger = logging.getLogger("detectron2")
-        if not logger.isEnabledFor(logging.INFO):
+        if not logger.isEnabledFor(logging.INFO):  # setup_logger is not called for d2
             setup_logger()
+        # Assume these objects must be constructed in this order.
         model = self.build_model(cfg)
         optimizer = self.build_optimizer(cfg, model)
-        model, optimizer = amp.initialize(model, optimizer, opt_level=optimization_level)
+        
+        model, optimizer = amp.initialize(model, optimizer, opt_level='O1')
+        
         data_loader = self.build_train_loader(cfg)
 
-        SimpleTrainer.__init__(self, model, data_loader, optimizer)
+        # For training, wrap with DDP. But don't need this for inference.
+        if comm.get_world_size() > 1:
+            model = DistributedDataParallel(
+                model, device_ids=[comm.get_local_rank()], broadcast_buffers=False
+            )
+        super().__init__(model, data_loader, optimizer)
 
         self.scheduler = self.build_lr_scheduler(cfg, optimizer)
+        # Assume no other objects need to be checkpointed.
+        # We can later make it checkpoint the stateful hooks
         self.checkpointer = DetectionCheckpointer(
+            # Assume you want to save checkpoints together with logs/statistics
             model,
             cfg.OUTPUT_DIR,
             optimizer=optimizer,
@@ -32,17 +47,38 @@ class ApexTrainer(DefaultTrainer):
         self.register_hooks(self.build_hooks())
 
     def run_step(self):
-        assert self.model.training
+        """
+        Implement the standard training logic described above.
+        """
+        assert self.model.training, "[SimpleTrainer] model was changed to eval mode!"
         start = time.perf_counter()
+        """
+        If your want to do something with the data, you can wrap the dataloader.
+        """
         data = next(self._data_loader_iter)
         data_time = time.perf_counter() - start
+
+        """
+        If your want to do something with the losses, you can wrap the model.
+        """
         loss_dict = self.model(data)
         losses = sum(loss for loss in loss_dict.values())
         self._detect_anomaly(losses, loss_dict)
+
         metrics_dict = loss_dict
         metrics_dict["data_time"] = data_time
         self._write_metrics(metrics_dict)
+
+        """
+        If you need accumulate gradients or something similar, you can
+        wrap the optimizer with your custom `zero_grad()` method.
+        """
         self.optimizer.zero_grad()
         with amp.scale_loss(losses, self.optimizer) as scaled_loss:
             scaled_loss.backward()
+
+        """
+        If you need gradient clipping/scaling or other processing, you can
+        wrap the optimizer with your custom `step()` method.
+        """
         self.optimizer.step()
