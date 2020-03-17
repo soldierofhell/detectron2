1927ac8e24a1d3d4b5bde0158ed232e6f7048ad7
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-03-17 17:19:02 +0100

    Update img2coco.py

--- projects/tracking/img2coco.py
+++ projects/tracking/img2coco.py
@@ -25,57 +25,54 @@ def get_img_dicts(img_dir):
 
   return dataset_dicts
 
-DatasetCatalog.clear()
-DatasetCatalog.register("inference_dataset", lambda: get_img_dicts('/content/LhcTGafSgw_all'))
-MetadataCatalog.get("inference_dataset").set(thing_classes=["person"])
+import json
+import os
 
 from detectron2.config import get_cfg
-
-cfg = get_cfg()
-
-# Cascade Mask RCNN ResNeXt 152
-cfg.merge_from_file("./detectron2_repo/configs/Misc/cascade_mask_rcnn_X_152_32x8d_FPN_IN5k_gn_dconv.yaml")
-cfg.MODEL.WEIGHTS = "/content/drive/My Drive/respo/MOT/cascade_rcnn/model_0018999.pth" # model_0005999 model_final.pth
-
-cfg.MODEL.MASK_ON = False
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
-
-cfg.DATASETS.TEST = ("inference_dataset",)
-cfg.DATALOADER.NUM_WORKERS = 4
-
 from detectron2.modeling import build_model
 from detectron2.checkpoint import DetectionCheckpointer
-
-model = build_model(cfg)
-model.eval()
-DetectionCheckpointer(model).load(cfg.MODEL.WEIGHTS)
-
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset, DatasetEvaluators
 from detectron2.data import build_detection_test_loader
 
-val_dataset = cfg.DATASETS.TEST[0]
-output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+def create_players_coco(image_dir, json_path, checkpoint, nms_threshold, detection_threshold, batch_size):
+  DatasetCatalog.clear()
+  DatasetCatalog.register("inference_dataset", lambda: get_img_dicts('/content/LhcTGafSgw_all'))
+  MetadataCatalog.get("inference_dataset").set(thing_classes=["person"])
 
-coco_data_loader = build_detection_test_loader(cfg, val_dataset) # batch_size = cfg.DATALOADER.NUM_WORKERS
-coco_evaluator = COCOEvaluator(val_dataset, cfg, False, output_folder)
+  cfg = get_cfg()
 
-inference_on_dataset(model, coco_data_loader, evaluator=coco_evaluator)
+  # Cascade Mask RCNN ResNeXt 152
+  cfg.merge_from_file("./detectron2_repo/configs/Misc/cascade_mask_rcnn_X_152_32x8d_FPN_IN5k_gn_dconv.yaml")
+  cfg.MODEL.WEIGHTS = checkpoint
+  cfg.MODEL.MASK_ON = False
+  cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
+  cfg.DATALOADER.NUM_WORKERS = batch_size
 
-import json
+  model = build_model(cfg)
+  model.eval()
+  DetectionCheckpointer(model).load(cfg.MODEL.WEIGHTS)
 
-with open('/content/output/inference/inference_dataset_coco_format.json', 'r') as f:
-  coco_dict = json.load(f)
+  val_dataset = "inference_dataset"
+  output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
 
-with open('/content/output/inference/coco_instances_results.json', 'r') as f:
-  annotations_list = json.load(f)
+  coco_data_loader = build_detection_test_loader(cfg, val_dataset) # batch_size = cfg.DATALOADER.NUM_WORKERS
+  coco_evaluator = COCOEvaluator(val_dataset, cfg, False, output_folder)
 
-for idx, ann in enumerate(annotations_list):
-  ann['id'] = idx
+  inference_on_dataset(model, coco_data_loader, evaluator=coco_evaluator)  
 
-coco_dict['annotations'] = annotations_list
+  with open(os.path.join(output_folder, 'inference_dataset_coco_format.json'), 'r') as f:
+    coco_dict = json.load(f)
 
-with open('/content/output/inference/inference_coco.json', 'w') as f:
-  json.dump(coco_dict, f)
+  with open(os.path.join(output_folder, 'coco_instances_results.json'), 'r') as f:
+    annotations_list = json.load(f)
+
+  for idx, ann in enumerate(annotations_list):
+    ann['id'] = idx
+
+  coco_dict['annotations'] = annotations_list
+
+  with open(json_path, 'w') as f:
+    json.dump(coco_dict, f)
   
 def parse_args():
   parser = argparse.ArgumentParser()  
@@ -90,4 +87,5 @@ def parse_args():
 
 
 if __name__ == "__main__":
-    args = parse_args()
+  args = parse_args()
+  create_players_coco(args.image_dir, args.json_path, args.checkpoint, args.nms_threshold, args.detection_threshold, args.batch_size)
